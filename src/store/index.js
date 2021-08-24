64e14166ee6ae8ac150def0f531071db8422ba1a
gregorius-tech <gregoriusverly@gmail.com>
2021-08-25 01:49:06 +0700

    set loginpage &router

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,30 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    isLogin: false,
+  },
+  mutations: {
+    LOGIN_STATUS(state, payload) {
+      state.isLogin = payload;
+    },
+  },
+  actions: {
+    async userLogin(context, payload) {
+      try {
+        const response = await localhost({
+          method: "post",
+          url: "/cust/login",
+          data: payload,
+        });
+
+        localStorage.setItem("access_token", response.data.access_token);
+        context.commit("LOGIN_STATUS", true);
+        router.push("/");
+      } catch (error) {
+        router.push("/login");
+      }
+    },
+  },
   modules: {},
 });
