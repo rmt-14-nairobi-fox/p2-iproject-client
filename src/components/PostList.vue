e462521288d39e8f87164528fa226f5b1f57dcb4
MarkTiger <marco.tiger.98@gmail.com>
2021-08-25 12:07:55 +0700

    Post list layout

---
+++ src/components/PostList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="col-3 custom-p">
+    <div class="bg-light custom-rounded post-height p-3 overflow-auto">
+      <div>
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+        test <br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+};
+</script>
+
+<style></style>
--- src/views/News.vue
+++ src/views/News.vue
@@ -2,17 +2,19 @@
   <div class="col mh-100">
     <div class="row">
       <NewsList />
-      <div class="col-3">test</div>
+      <PostList />
     </div>
   </div>
 </template>
 
 <script>
 import NewsList from '../components/NewsList.vue';
+import PostList from '../components/PostList.vue';
 export default {
   name: 'News',
   components: {
     NewsList,
+    PostList,
   },
 };
 </script>
@@ -35,7 +37,19 @@ export default {
   height: calc(100vh - 201.8px);
 }
 
+.post-height {
+  height: calc(100vh - 117.8px);
+}
+
 .custom-pt {
   padding-top: 16px;
+}
+
+.custom-p {
+  padding: 16px 16px 16px 0;
+}
+
+.custom-rounded {
+  border-radius: 12px;
 }
 </style>
