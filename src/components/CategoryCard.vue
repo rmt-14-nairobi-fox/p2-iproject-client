fbd6aff0d3ea51d3ea3366665ba312e847b72932
handiAngga <wadangalap@gmail.com>
2021-08-25 14:29:50 +0700

    add home

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-3">
+        <div class="card my-2">
+            <div class="card-body">
+                <a @click.prevent="categoryDetail(category.slug)" href="" class="text-decoration-none text-dark" >
+                    <h5 class="card-title">{{category.attributes.name}}</h5>
+                </a>
+            </div>
+        </div>
+        <!-- <pre>{{JSON.stringify(category,null,2)}}</pre> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name:"CategoryCard",
+    props:["category"],
+    methods:{
+        categoryDetail(slug){
+            console.log(slug);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top" id="mainNav">
+        <div class="container px-4">
+            <img src="../assets/drugs.png" style="height:60px; width:60px;">
+            <h4>CariObat</h4>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item "><a class="nav-link text-dark" href="#about">Favorite</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#services">Login</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#contact">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
