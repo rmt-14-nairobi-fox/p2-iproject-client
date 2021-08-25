1d95b87cd231ba27575135d7211e6f9b94e7ad30
MarkTiger <marco.tiger.98@gmail.com>
2021-08-25 18:27:47 +0700

    Search box done

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="col-9 custom-pt">
     <div class="d-flex flex-column mw-100">
-      <input
-        type="text"
-        name="search"
-        id="search"
-        placeholder="Search news"
-        class="search-box p-3 text-center"
-      />
+      <form class="search-box" @submit.prevent="searchAction">
+        <input
+          v-model="searchQuery"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search news"
+          class="search-box p-3 w-100 text-center"
+        />
+      </form>
+
       <div class="bg-light mw-100 item-height px-3 overflow-auto">
         <div class="row mt-3">
           <NewsCard
@@ -26,6 +30,11 @@ import NewsCard from '../components/NewsCard.vue';
 import { mapState } from 'vuex';
 export default {
   name: 'NewsList',
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
   computed: {
     ...mapState(['news']),
   },
@@ -35,6 +44,13 @@ export default {
   methods: {
     fetchNews() {
       this.$store.dispatch('fetchNews');
+    },
+    searchAction() {
+      if (this.searchQuery) {
+        this.$store.dispatch('searchAction', { searchQuery: this.searchQuery });
+      } else {
+        this.fetchNews();
+      }
     },
   },
   created() {
