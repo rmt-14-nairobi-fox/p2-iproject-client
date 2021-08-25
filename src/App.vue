26d2e5244be37f322e4972f0f816877a326571ee
gregorius-tech <gregoriusverly@gmail.com>
2021-08-25 12:20:31 +0700

    midtrans clearr

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <Navbar></Navbar> -->
+    <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+
     <router-view />
   </div>
 </template>
-
+<script>
+// import Navbar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    // Navbar,
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
